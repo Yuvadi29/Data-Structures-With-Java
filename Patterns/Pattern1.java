fbe51cb9251dde563d449ee67a5f2241709e9355
Yuvadi29 <adityatrivedi612@gmail.com>
2022-12-14 22:42:36 +0530

    Pattern1

--- Patterns/Pattern1.java
+++ Patterns/Pattern1.java
@@ -13,11 +13,12 @@ class Pattern1 {
         System.out.println("Enter the Number of Rows: ");
         int n = sc.nextInt(); // Taking the number of rows as input
 
-        int i = 1; // Initialising i as row
+        int i = 1; // Initialising i as row, Bolega ppehli row se start karte hai
         while (i <= n) {
             // Har row ke liye chalega i, first second third likewise
             int j = 1;
             while (j <= n) {
+                //Har column ke liye chalega j
                 System.out.print("*");
                 j++;
             }
