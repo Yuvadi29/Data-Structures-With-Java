4f760220af8da2245df431b48b6a9c5057a14066
Yuvadi29 <adityatrivedi612@gmail.com>
2022-12-21 13:47:33 +0530

    Pattern7

---
+++ Patterns/Pattern7.java
@@ -0,0 +1,24 @@
+/*Simple Star Pattern with equal rows and columns is printed here 
+1
+2 2
+3 3 3
+4 4 4 4
+*/
+
+import java.util.*;
+
+class Pattern7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+    }
+}
