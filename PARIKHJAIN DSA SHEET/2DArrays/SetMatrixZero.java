32e3e2e7ea7ea50af0a47b31ca8373a6d8df53e5
Yuvadi29 <adityatrivedi612@gmail.com>
2023-07-03 22:16:19 +0530

    Set Max Zeroes

---
+++ PARIKHJAIN DSA SHEET/2DArrays/SetMatrixZero.java
@@ -0,0 +1,81 @@
+/*We are given a matrix of N*M. If an element  is 0, set its entire row and column to 0 and return the matrix. Use Inplace operation 
+ * Approach:
+ * Iterate through the array
+Check if the element is 0
+If it is zero:
+Loop its column and row and make it 0
+ */
+
+import java.util.*;
+
+public class SetMatrixZero {
+
+    public static void setZeroes(int matrix[][]) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        // Check if the first row has zero
+        for (int j = 0; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                firstRowHasZero = true;
+                break;
+            }
+        }
+
+        // Check if the first column has zero
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
+
+        // Use the first row and first column as markers for zeroing
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0; // Mark the corresponding row
+                    matrix[0][j] = 0; // Mark the corresponding column
+                }
+            }
+        }
+
+        // Zero out rows based on the first column marker
+        for (int i = 1; i < rows; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < cols; j++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out columns based on the first row marker
+        for (int j = 1; j < cols; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < rows; i++) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        // Zero out the first row if needed
+        if (firstRowHasZero) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        // Zero out the first column if needed
+        if (firstColHasZero) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+
+    }
+
+}
