2e374d5e1c6c36ef38ac613a1c8f10a949f32b45
Yuvadi29 <adityatrivedi612@gmail.com>
2023-09-14 14:41:58 +0530

    Singly LinkedList Syntax

---
+++ LinkedList/SinglyLinkedList.java
@@ -0,0 +1,17 @@
+/*Singly LinkedList */
+
+public class SinglyLinkedList {
+    
+
+    // Defining a singly linkedlist
+    public class SinglyListNode {
+        // Current Value
+        int val;
+        // Next Node of the LinkedList
+        SinglyListNode next;
+        SinglyListNode(int x) {
+            val = x;
+        }
+
+    }
+}
