95d5f699caafd1ccb3ad4db7cc2f0af15765f90e
Yuvadi29 <adityatrivedi612@gmail.com>
2023-12-03 13:23:48 +0530

    Introduction to Doubly LinkedLists ✅

---
+++ LinkedList/DoublyLinkedList.java
@@ -0,0 +1,13 @@
+/*Doubly LinkedList are the linkedLists which contains a prev node and a next node as well. With this we are able to know the previous node of the current node. */
+
+public class DoublyLinkedList {
+    
+    class DoublyLinkedList {
+        int val;
+        DoublyLinkedList next, prev;
+        
+        DoublyLinkedList(int x){
+            this.val = x;
+        }
+    }
+}
