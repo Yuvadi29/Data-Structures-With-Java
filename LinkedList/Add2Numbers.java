27ca662351b0d222bf96b2d84796e94b7c515b12
Yuvadi29 <adityatrivedi612@gmail.com>
2024-02-01 11:55:21 +0530

    Adding 2 Numbers in LinkedList ✅

---
+++ LinkedList/Add2Numbers.java
@@ -0,0 +1,45 @@
+/*
+ * We are given 2 numbers in reversed form, we have to add the numbers and
+ * return the value in the form of linkedList
+ */
+
+public class Add2Numbers {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        };
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = (l1 != null) ? l1.val : 0;
+            int y = (l2 != null) ? l2.val : 0;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null)
+                l1 = l1.next;
+            if (l2 != null)
+                l2 = l2.next;
+        }
+        return dummy.next;
+    }
+}
